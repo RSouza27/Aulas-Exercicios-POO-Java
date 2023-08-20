c143bc06d4ccbe21df4f4fd7aaad023e85982d62
Renan Souza <114888859+RSouza27@users.noreply.github.com>
2023-08-20 18:31:17 -0300

    Classe Main Aluno / Objeto Aluno

---
+++ Aula1POO/src/POOExercicio2Aluno/Aluno.java
@@ -0,0 +1,88 @@
+package POOExercicio2Aluno;
+
+public class Aluno {
+    private String nome;
+    private int ra;
+    private double ac1;
+    private double ac2;
+    private double ag;
+    private double af;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public double getAc1() {
+        return ac1;
+    }
+
+    public void setAc1(double ac1) {
+        this.ac1 = ac1;
+    }
+
+    public double getAc2() {
+        return ac2;
+    }
+
+    public void setAc2(double ac2) {
+        this.ac2 = ac2;
+    }
+
+    public double getAg() {
+        return ag;
+    }
+
+    public void setAg(double ag) {
+        this.ag = ag;
+    }
+
+    public double getAf() {
+        return af;
+    }
+
+    public void setAf(double af) {
+        this.af = af;
+    }
+    
+    //==================================================
+    
+    public double calcularMedia() {    
+     
+        double media = ac1 * 0.15 + ac2 * 0.30 + ag * 0.10 + af * 0.45;
+        
+        return media;
+    } 
+    
+    public String verificarAprovacao() {
+        if(calcularMedia() >= 5){
+            return "Aprovado";
+        }else{
+            return "Reprovado";
+        }
+    } 
+    
+    public String imprimirAluno() {
+        String str = "Nome: "  + getNome() +
+                     "\nRA: "  + getRa() +
+                     "\nAC1: " + getAc1() +
+                     "\nAC2: " + getAc2() +
+                     "\nAG: "  + getAg() +
+                     "\nAF: "  + getAf() +
+                     "\nMÉDIA: "  + calcularMedia() +
+                     "\nSITUAÇÃO: " + verificarAprovacao() ;
+        
+        return str;
+    } 
+}
---
+++ Aula1POO/src/POOExercicio2Aluno/AlunoMediaExercicio2.java
@@ -0,0 +1,56 @@
+package POOExercicio2Aluno;
+
+import POOExercicio2Aluno.Aluno;
+import javax.swing.JOptionPane;
+
+public class AlunoMediaExercicio2 {
+
+    public static void main(String[] args) {
+        String nome, opt = "";
+        int ra;
+        double ac1, ac2, ag, af;
+        Aluno aluno = new Aluno();
+        
+        do {
+            opt = JOptionPane.showInputDialog("1 - Cadastrar Aluno \n2- Visualizar Aluno \n3- Sair");
+            
+            if(opt == null){
+                System.exit(0);
+            }
+            
+            switch(opt) {
+                case "1":
+                    //Cadastrar
+                    nome = JOptionPane.showInputDialog("Digite seu Nome: ");
+                    ra  = Integer.parseInt(JOptionPane.showInputDialog("Digite seu RA: ")); 
+                    ac1 = Double.parseDouble(JOptionPane.showInputDialog("Nota AC1: "));
+                    ac2 = Double.parseDouble(JOptionPane.showInputDialog("Nota AC2: "));
+                    ag  = Double.parseDouble(JOptionPane.showInputDialog("Nota AG:" ));
+                    af  = Double.parseDouble(JOptionPane.showInputDialog("Nota AF: "));
+                         
+                    aluno = new Aluno();
+                    aluno.setNome(nome);
+                    aluno.setRa(ra);
+                    aluno.setAc1(ac1);
+                    aluno.setAc2(ac2);
+                    aluno.setAg(ag);
+                    aluno.setAf(af);
+                    break;
+                case "2":
+                    //Visualizar
+                    if(aluno == null){
+                        JOptionPane.showMessageDialog(null, "Nenhuma Pessoa Cadastrada!");
+                        break;
+                    }
+                    
+                    JOptionPane.showMessageDialog(null, aluno.imprimirAluno());
+                    break;
+                case "3":
+                    //Sair
+                    break; 
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida!", "Erro", JOptionPane.WARNING_MESSAGE);
+            }           
+        }while(!opt.equals("3"));
+    }     
+}
