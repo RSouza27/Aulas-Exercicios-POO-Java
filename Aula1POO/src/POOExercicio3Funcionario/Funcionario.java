7d338b75ff4d27a1cfff84046fa49660d24b086b
Renan Souza <114888859+RSouza27@users.noreply.github.com>
2023-08-24 20:08:08 -0300

    Create Main Class and Class Funcionario

--- Aula1POO/src/POOExercicio3Funcionario/Funcionario.java
+++ Aula1POO/src/POOExercicio3Funcionario/Funcionario.java
@@ -1,13 +1,100 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package POOExercicio3Funcionario;
 
-/**
- *
- * @author Leandro
- */
 public class Funcionario {
+    private String nome;
+    private int cracha;
+    private int tipoVinculo;
+    private double valorHora;
+    private double qntdHora;
+    private double salario;
+    private double valorDesconto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCracha() {
+        return cracha;
+    }
+
+    public void setCracha(int cracha) {
+        this.cracha = cracha;
+    }
+
+    public int getTipoVinculo() {
+        return tipoVinculo;
+    }
+
+    public void setTipoVinculo(int tipoVinculo) {
+        this.tipoVinculo = tipoVinculo;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public double getQntdHora() {
+        return qntdHora;
+    }
+
+    public void setQntdHora(double qntdHora) {
+        this.qntdHora = qntdHora;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(double valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
     
+    public double calcularSalario() {
+        if(tipoVinculo == 0){
+            return valorHora * qntdHora;
+        }else 
+            return salario;
+    }        
+    
+    public double valorReceber(){
+        if (tipoVinculo == 0){
+            return (valorHora * qntdHora) - valorDesconto; 
+        }
+        return salario - valorDesconto;  
+    }
+    
+    public String exibirVinculo() {
+        if (tipoVinculo == 0) {
+            return "Horista";
+        } else {
+            return "Normal";
+        }
+    }
+    
+    public String imprimirFuncionario() {
+        String str = "Nome: "  + getNome() +
+                     "\nNumero do Cracha: "  + getCracha() +
+                     "\nTipo de Vinculo: " + exibirVinculo() +
+                     "\nSalário: "  + calcularSalario() +
+                     "\nDesconto do Salário: "  + getValorDesconto() + 
+                     "\nValor a Receber: "  + valorReceber();
+        
+        return str;
+    } 
 }
--- Aula1POO/src/POOExercicio3Funcionario/MainFuncionario.java
+++ Aula1POO/src/POOExercicio3Funcionario/MainFuncionario.java
@@ -1,20 +1,84 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package POOExercicio3Funcionario;
 
-/**
- *
- * @author Leandro
- */
+import javax.swing.JOptionPane;
+
 public class MainFuncionario {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        String nome, opt = "";
+        int cracha, tipoVinculo;
+        double valorHora = 0;
+        double qntdHora = 0;
+        double salario = 0;
+        double valorDesconto = 0;
+        
+        String[] opcoes = {"Horista", "Normal"};
+        
+        Funcionario funcionario = new Funcionario();
+        
+        do {
+            opt = JOptionPane.showInputDialog("1 - Cadastrar Funcionario \n2 - Mostrar Folha de Pagamento "
+                    + "                       \n3 - Alterar remuneração \n4 - Sair");
+            
+            if(opt == null){
+                System.exit(0);
+            }
+            
+            switch(opt) {
+                case "1":
+                    //Cadastrar
+                    nome = JOptionPane.showInputDialog("Digite seu Nome: ");
+                    cracha = Integer.parseInt(JOptionPane.showInputDialog("Digite numero do seu Cracha: ")); 
+                    tipoVinculo = JOptionPane.showOptionDialog(null, "Qual o tipo de vínculo?",
+                        "Escolha uma opção",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+                    if(tipoVinculo == 0){
+                        valorHora = Double.parseDouble(JOptionPane.showInputDialog("Digite o Valor por Hora Trabalhada: "));
+                        qntdHora  = Double.parseDouble(JOptionPane.showInputDialog("Digite a Quantidade de Horas Trabalhadas:" ));
+                    }else {
+                        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite seu Salário: "));
+                    }     
+                    valorDesconto = Double.parseDouble(JOptionPane.showInputDialog("Digite o Desconto de Salário: "));
+                         
+                    funcionario = new Funcionario();
+                    funcionario.setNome(nome);
+                    funcionario.setCracha(cracha);
+                    funcionario.setTipoVinculo(tipoVinculo);
+                    funcionario.setValorHora(valorHora);
+                    funcionario.setQntdHora(qntdHora);
+                    funcionario.setSalario(salario);
+                    funcionario.setValorDesconto(valorDesconto);
+                    break;
+                    
+                case "2":
+                    //Visualizar
+                    if(funcionario == null){
+                        JOptionPane.showMessageDialog(null, "Nenhum Funcionário Cadastrado!");
+                        break;
+                    }
+                    
+                    JOptionPane.showMessageDialog(null, funcionario.imprimirFuncionario());
+                    break;
+                    
+                case "3":    
+                    if (funcionario.getTipoVinculo() == 0) {
+                        valorHora = Double.parseDouble(JOptionPane.showInputDialog("Digite o novo valor da hora: "));
+                        qntdHora = Double.parseDouble(JOptionPane.showInputDialog("Digite o novo total de horas: "));    
+                    } else {
+                        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o novo salário: "));
+                    }
+                    
+                    funcionario.setValorHora(valorHora);
+                    funcionario.setQntdHora(qntdHora);
+                    funcionario.setSalario(salario);
+                    break;
+                    
+                case "4":
+                    //Sair
+                    break; 
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida!", "Erro", JOptionPane.WARNING_MESSAGE);
+            }           
+        }while(!opt.equals("4"));
+    }         
 }
